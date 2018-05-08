6be4fcd21d86f843af2c74df53b86977098166a2
CodeDad <github email address>
2018-05-07 23:24:52 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1> There doesn't seem to be a lab associated with this Read Me quite yet...</h1>
       </div>
     );
   }
